03632da527a0dcf77de139d72b570dc3d116ec1f
mosulishvili <62496976+mosulishvili@users.noreply.github.com>
2026-08-28 15:37:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-TELEGRAM_BOT_TOKEN = "8782914659:AAFYct1Jp3ypWM37ch40-6W4T8g16_Uf5_w"
+TELEGRAM_BOT_TOKEN = "8782914659:AAFYct1Jp3ypWM3"
 CHAT_ID = "898291410"
 
 def send_wizzair_alert(flight_date, price_gel):
@@ -11,19 +11,17 @@ def send_wizzair_alert(flight_date, price_gel):
         f"💰 ფასი: {price_gel} ₾\n\n"
         f"🔗 https://wizzair.com"
     )
-    
+
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {
         "chat_id": CHAT_ID,
         "text": message
     }
-    
+
     response = requests.post(url, json=payload)
     if response.status_code == 200:
         print("შეტყობინება გაიგზავნა!")
     else:
         print("შეცდომა:", response.text)
 
-if __name__ == "__main__":
-    send_wizzair_alert("2026-09-15", 140)
-  
+send_wizzair_alert("2026-10-15", "149")
